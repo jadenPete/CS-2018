0dedc102ebbabcebc0fbb973120ed7cdb0370681
Jaden Peterson <jadenpeterson150@gmail.com>
2018-05-16 16:43:58 -0700

    Less hackish

--- scripts/3d-printing.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  var content = $(".mdl-layout__content");
-  var headerImage = $(".header-image");
-
-  content.scroll(function(){
-    headerImage.css("height", "calc(40vh - " + content.scrollTop() + "px)");
-  });
-});
